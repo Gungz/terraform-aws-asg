3b138e54fa9053c8acc072af85653cd9a633b293
Agung Sidharta So <agung_sidharta_gungz@yahoo.com>
2022-07-06 20:53:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "aws_launch_configuration" "this" {
   count = var.create_lc ? 1 : 0
 
   name_prefix                 = "${coalesce(var.lc_name, var.name)}-"
-  image_id                    = "${var.image_id}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.iam_instance_profile}"
-  key_name                    = "${var.key_name}"
+  image_id                    = var.image_id
+  instance_type               = var.instance_type
+  iam_instance_profile        = var.iam_instance_profile
+  key_name                    = var.key_name
   security_groups             = var.security_groups
-  associate_public_ip_address = "${var.associate_public_ip_address}"
-  user_data                   = "${var.user_data}"
-  enable_monitoring           = "${var.enable_monitoring}"
-  spot_price                  = "${var.spot_price}"
+  associate_public_ip_address = var.associate_public_ip_address
+  user_data                   = var.user_data
+  enable_monitoring           = var.enable_monitoring
+  spot_price                  = var.spot_price
   placement_tenancy           = "${var.spot_price == "" ? var.placement_tenancy : ""}"
-  ebs_optimized               = "${var.ebs_optimized}"
+  ebs_optimized               = var.ebs_optimized
   
   dynamic "ebs_block_device" {
     for_each = var.ebs_block_device
@@ -67,32 +67,32 @@ resource "aws_autoscaling_group" "this" {
   name_prefix          = "${join("-", compact(tolist([coalesce(var.asg_name, var.name), var.recreate_asg_when_lc_changes ? element(concat(random_pet.asg_name.*.id, tolist([""])), 0) : ""])))}-"
   launch_configuration = "${var.create_lc ? element(aws_launch_configuration.this.*.name, 0) : var.launch_configuration}"
   vpc_zone_identifier  = var.vpc_zone_identifier
-  max_size             = "${var.max_size}"
-  min_size             = "${var.min_size}"
-  desired_capacity     = "${var.desired_capacity}"
+  max_size             = var.max_size
+  min_size             = var.min_size
+  desired_capacity     = var.desired_capacity
 
   load_balancers            = var.load_balancers
-  health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type         = "${var.health_check_type}"
+  health_check_grace_period = var.health_check_grace_period
+  health_check_type         = var.health_check_type
 
-  min_elb_capacity          = "${var.min_elb_capacity}"
-  wait_for_elb_capacity     = "${var.wait_for_elb_capacity}"
+  min_elb_capacity          = var.min_elb_capacity
+  wait_for_elb_capacity     = var.wait_for_elb_capacity
   target_group_arns         = var.target_group_arns
-  default_cooldown          = "${var.default_cooldown}"
-  force_delete              = "${var.force_delete}"
-  termination_policies      = "${var.termination_policies}"
-  suspended_processes       = "${var.suspended_processes}"
-  placement_group           = "${var.placement_group}"
+  default_cooldown          = var.default_cooldown
+  force_delete              = var.force_delete
+  termination_policies      = var.termination_policies
+  suspended_processes       = var.suspended_processes
+  placement_group           = var.placement_group
   enabled_metrics           = var.enabled_metrics
-  metrics_granularity       = "${var.metrics_granularity}"
-  wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-  protect_from_scale_in     = "${var.protect_from_scale_in}"
+  metrics_granularity       = var.metrics_granularity
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout
+  protect_from_scale_in     = var.protect_from_scale_in
 
-  tags = ["${concat(
+  tags = concat(
       tolist([tomap({"key" = "Name", "value" = var.name, "propagate_at_launch" = true})]),
       var.tags,
       local.tags_asg_format
-   )}"]
+   )
 
   lifecycle {
     create_before_destroy = true
@@ -100,7 +100,7 @@ resource "aws_autoscaling_group" "this" {
 }
 
 resource "random_pet" "asg_name" {
-  count = "${var.recreate_asg_when_lc_changes ? 1 : 0}"
+  count = var.recreate_asg_when_lc_changes ? 1 : 0
 
   separator = "-"
   length    = 2
